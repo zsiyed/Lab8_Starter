edf8241833988080c0d7d0eed3ea2b535b81ce2f
Camdyn Rasque <rasque.camdyn@gmail.com>
2021-11-10 11:51:39 -0800

    Added unit tests

---
+++ __tests__/unit.test.js
@@ -0,0 +1,5 @@
+// unit.test.js
+
+const functions = require('../code-to-unit-test/unit-test-me.js');
+
+// TODO - Part 2
---
+++ code-to-unit-test/unit-test-me.js
@@ -0,0 +1,36 @@
+// unit-test-me.js
+
+module.exports = { isPhoneNumber, isEmail, isStrongPassword, isDate, isHexColor };
+
+// matches valid phone numbers
+function isPhoneNumber(phoneNumber) {
+  const phoneRegex = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/;
+  return phoneRegex.test(phoneNumber);
+}
+
+// matches valid emails
+function isEmail(email) {
+  const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  return emailRegex.test(email);
+}
+
+/**
+ * The password's first character must be a letter, it must contain at least * 4 characters and no more than 15 characters and no characters other than * * letters, numbers and the underscore may be used
+ */
+function isStrongPassword(password) {
+  const pwRegex = /^[a-zA-Z]\w{3,14}$/;
+  return pwRegex.test(password);
+}
+
+// 	This regular expressions matches dates of the form XX / XX / YYYY where 
+// XX can be 1 or 2 digits long and YYYY is always 4 digits long.
+function isDate(date) {
+  const dateRegex = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+  return dateRegex.test(date);
+}
+
+// Matches valid 3 or 6 character hex codes used for HTML or CSS.
+function isHexColor(color) {
+  const colorRegex = /^\#?[A-Fa-f0-9]{3}([A-Fa-f0-9]{3})?$/;
+  return colorRegex.test(color);
+}
